358006a50b825c7f44163f8995d66a8e15446f59
rohitroxz3700 <rohitroxz3700@gmail.com>
2025-03-30 00:47:19 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import axios from "axios"
+import { useNavigate } from "react-router-dom";
 function App() {
+
+  const navigate = useNavigate()
+  const [user,setuser] = useState("")
+  const [pass,setpass] = useState("")
+
+  function handleuser(event)
+  {
+    setuser(event.target.value)
+  }
+  function handlepass(event)
+  {
+    setpass(event.target.value)
+  }
+  function check()
+  {
+    //let logindetails = axios.get(`http://localhost:5000/login?username=${user}&password=${pass}`)
+    let logindetails = axios.post("http://localhost:5000/login",{"username":user,"password":pass})
+    logindetails.then(function(data){
+      console.log(data)
+      if(data.data === "Login Successful")
+      {
+        navigate("/success")
+      }
+      else
+      {
+        navigate("/fail")
+      }
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input placeholder="username" name="username" onChange={handleuser} value={user}></input>
+      <input placeholder="password" name="password" onChange={handlepass} value={pass}></input>
+      <button onClick={check}>Login</button>
     </div>
   );
 }
---
+++ src/Success.js
@@ -0,0 +1,8 @@
+function Success()
+{
+    return(
+        <h1>Login Successful</h1>
+    )
+}
+
+export default Success
